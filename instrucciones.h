afddee3fbc58ad2072101e1b5036e81e2ba51a0f
Luichi <ltitaewatanabe@frba.utn.edu.ar>
2024-08-20 00:36:22 -0300

    declaracion de fuciones en el header

--- instrucciones.h
+++ instrucciones.h
@@ -10,4 +10,8 @@
 
 //extern int cubo_rubik[6][3][3];
 
+void string_a_caracteres(char input[100]);
+int es_numero(char caracter);
+void caracter_a_instruccion(char caracter);
+
 #endif // !INSTRUCCION
